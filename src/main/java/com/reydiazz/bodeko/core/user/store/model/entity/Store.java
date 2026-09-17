f8fa0495bc99f613722c1aa7b7c99739618c4e69
Fabrizio7S <leon.mallma14@gmail.com>
2026-09-17 11:08:06 -0500

    feat(user): agrega la entidad tienda en el modulo usuario

---
+++ src/main/java/com/reydiazz/bodeko/core/user/store/model/entity/Store.java
@@ -0,0 +1,31 @@
+package com.reydiazz.bodeko.core.user.store.model.entity;
+
+import com.reydiazz.bodeko.core.user.core.model.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@Entity
+@Getter
+@Table(name = "stores")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Store {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "subdomain", unique = true)
+    private String subdomain;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
