4a61b6870677acc21ef487628d2af0a5e7caae7e
Fabrizio7S <leon.mallma14@gmail.com>
2026-09-17 16:06:56 -0500

    remake(user): cambio de posicion de la carpeta repository

--- src/main/java/com/reydiazz/bodeko/core/user/store/repository/StoreRepository.java
+++ src/main/java/com/reydiazz/bodeko/core/user/store/repository/StoreRepository.java
@@ -1,4 +1,4 @@
-package com.reydiazz.bodeko.core.user.store.model.repository;
+package com.reydiazz.bodeko.core.user.store.repository;
 
 import com.reydiazz.bodeko.core.user.store.model.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
