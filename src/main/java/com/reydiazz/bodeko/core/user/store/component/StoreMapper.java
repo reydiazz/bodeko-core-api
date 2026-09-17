feb7681212800f677be407b11460ec0c46732f8e
Fabrizio7S <leon.mallma14@gmail.com>
2026-09-17 16:53:34 -0500

    remake(user): agregar anotaciones en el mapper tienda

--- src/main/java/com/reydiazz/bodeko/core/user/store/component/StoreMapper.java
+++ src/main/java/com/reydiazz/bodeko/core/user/store/component/StoreMapper.java
@@ -2,7 +2,11 @@ package com.reydiazz.bodeko.core.user.store.component;
 
 import com.reydiazz.bodeko.core.user.store.model.entity.Store;
 import com.reydiazz.bodeko.core.user.store.web.response.StoreResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class StoreMapper {
 
     public StoreResponse toResponse(Store store){
