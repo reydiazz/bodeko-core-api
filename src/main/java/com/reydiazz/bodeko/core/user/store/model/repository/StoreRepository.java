029d0495db1cc52b47d019f274efb094b5c40ffd
Fabrizio7S <leon.mallma14@gmail.com>
2026-09-17 11:16:00 -0500

    feat(user): agrega el repositorio tienda en el modulo usuario

---
+++ src/main/java/com/reydiazz/bodeko/core/user/store/model/repository/StoreRepository.java
@@ -0,0 +1,12 @@
+package com.reydiazz.bodeko.core.user.store.model.repository;
+
+import com.reydiazz.bodeko.core.user.store.model.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, UUID> {
+
+}
