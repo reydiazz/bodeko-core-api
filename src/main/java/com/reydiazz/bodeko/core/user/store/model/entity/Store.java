f4250f244193341ee9b0434aa8800331da1331d2
Fabrizio7S <leon.mallma14@gmail.com>
2026-09-17 23:15:57 -0500

    refactor(user): corregir metodo update tienda

--- src/main/java/com/reydiazz/bodeko/core/user/store/model/entity/Store.java
+++ src/main/java/com/reydiazz/bodeko/core/user/store/model/entity/Store.java
@@ -28,9 +28,8 @@ public class Store {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    private void update(String name , String subdomain) {
+    public void update(String name) {
         this.name = name;
-        this.subdomain = subdomain;
     }
 
 }
