8105dba7894e4ed8fab2fff1a69cd38c743e467b
Fabrizio7S <leon.mallma14@gmail.com>
2026-09-22 00:28:19 -0500

    feat(product): agrega las entidades de productos

---
+++ src/main/java/com/reydiazz/bodeko/core/product/core/model/entity/Product.java
@@ -0,0 +1,54 @@
+package com.reydiazz.bodeko.core.product.core.model.entity;
+
+
+import com.reydiazz.bodeko.core.user.store.model.entity.Store;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Builder
+@Entity
+@Getter
+@Table(name = "products")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Product {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description",columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "price",nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "stock",nullable = false)
+    private Integer stock;
+
+    @Column(name = "imagen_url",columnDefinition = "TEXT")
+    private String imagenUrl;
+
+    @Column(name = "status")
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    public  void  update(String name,String description,BigDecimal price,String imagenUrl,String status) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imagenUrl = imagenUrl;
+        this.status = status;
+    }
+
+
+}
