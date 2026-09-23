2a9c1db83a9434d89d53bd02a776fc4508c28061
Fabrizio7S <leon.mallma14@gmail.com>
2026-09-22 19:53:19 -0500

    feat(product): agrega las entidades categoria al modulo producto

---
+++ src/main/java/com/reydiazz/bodeko/core/product/categories/entity/Category.java
@@ -0,0 +1,32 @@
+package com.reydiazz.bodeko.core.product.categories.entity;
+
+import com.reydiazz.bodeko.core.user.store.model.entity.Store;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@Entity
+@Getter
+@Table(name = "categories")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Category {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    public  void update (UUID id,String name){
+        this.id=id;
+        this.name=name;
+    }
+}
